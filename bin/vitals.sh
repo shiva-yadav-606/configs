5cea5e23543a91f290ed807c3e35bc76fb30a42f
shiva-yadav-606 <shivayadav.123.007@gmail.com>
2026-02-01 16:03:23 +0530

    push

--- bin/vitals.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo watts      = $(battery_watt.sh)
-echo picom      = $(compositor.sh)
-echo fan speed  = $(fan_speed.sh)
-echo gpu usage  = $(gpu_util.sh)
-echo nmve temp  = $(nvme_temp.sh)
-echo mode       = $(power_profile.sh)
